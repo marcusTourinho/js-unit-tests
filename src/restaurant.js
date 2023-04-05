e899895302b313730c2e1da729b7406b07eeac28
Marcus Vinicius de Jesus Tourinho <viniciustourinho01@gmail.com>
2023-04-05 17:46:49 -0300

    Requisito 10

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,6 +1,31 @@
 /* eslint-disable max-len */
 // Siga as orientações do README!
 
-const createMenu = () => {};
+const createMenu = (object) => {
+  const fetchMenu = () => object;
+  const consumption = [];
+  const order = (string) => {
+    if (object.drinks[string]) {
+      consumption.push(string);
+    }
+    if (object.food[string]) {
+      consumption.push(string);
+    }
+    return 'Item indisponível';
+  }
+  const pay = () => {
+    let tip = 0;
+    for (let key of consumption) {
+      if (object.drinks[key]) {
+        tip += object.drinks[key];
+      }
+      if (object.food[key]) {
+        tip += object.food[key];
+      }
+    }
+    return tip * 1.1;
+  }
+  return {fetchMenu, consumption, order, pay};
+};
 
 module.exports = createMenu;
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -1,8 +1,30 @@
 const createMenu = require('../src/restaurant');
- 
+
+const menu = {
+  food: {coxinha: 3.90, sanduiche: 9.90},
+  drinks: {agua: 3.90, cerveja: 6.90},
+}
+
+const myMenu = createMenu(menu);
+
 describe('10 - Implemente a função `createMenu`, bem como seus casos de teste', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
+    expect(myMenu).toHaveProperty('fetchMenu');
+    expect(myMenu.fetchMenu).toBeInstanceOf(Function);
+    expect(Object.keys(myMenu.fetchMenu())).toStrictEqual([ 'food', 'drinks' ]);
+    expect(menu).toStrictEqual({
+      food: {coxinha: 3.90, sanduiche: 9.90},
+      drinks: {agua: 3.90, cerveja: 6.90},
+    });
+    expect(myMenu.consumption).toHaveLength(0);  
+    expect(myMenu.order('calabresa-apimentada')).toBe('Item indisponível');
+    myMenu.order('agua');
+    myMenu.order('coxinha');
+    myMenu.order('sanduiche');
+    myMenu.order('agua');
+    myMenu.order('alirio');
+    expect(myMenu.consumption).toHaveLength(4);
+    expect(myMenu.pay()).toBeCloseTo(21.60 * 1.1);
     // Escreva todos os testes aqui.
   });
 });
